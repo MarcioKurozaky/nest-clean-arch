adf8104f4fe01198efb6571dedf71bc450488cfc
marcioKurozaky <marciode2014@gmail.com>
2026-01-02 23:12:53 +0100

    getMethod unit test

--- src/users/domain/entities/user.entity.ts
+++ src/users/domain/entities/user.entity.ts
@@ -10,4 +10,24 @@ export class UserEntity {
   constructor(public readonly props: UserProps) {
     this.props.createdAt = this.props.createdAt ?? new Date()
   }
+
+  get name() {
+    return this.props.name
+  }
+
+  get email() {
+    return this.props.email
+  }
+
+  get phoneNumber() {
+    return this.props.phoneNumber
+  }
+
+  get password() {
+    return this.props.password
+  }
+
+  get createdAt() {
+    return this.props.createdAt
+  }
 }
